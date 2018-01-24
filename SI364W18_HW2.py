06c3a938c424371399c96e34a5006aab2178fc62
Allan Chen <allanmc@umich.edu>
2018-01-24 15:35:31 -0500

    stable, but CSRF not matching first time, second time submission successful

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -11,22 +11,37 @@
 #############################
 ##### IMPORT STATEMENTS #####
 #############################
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, flash, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, ValidationError
+from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import Required
 
+
+
+
+
+
+
+
+
+import requests
+import json
 #####################
 ##### APP SETUP #####
 #####################
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hardtoguessstring'
+app.debug=True
+
 
 ####################
 ###### FORMS #######
 ####################
-
+class AlbumEntryForm(FlaskForm):
+    album_name = StringField('Enter the name of an album: ', validators=[Required()])
+    like = RadioField("How much do you like this album? (1 low, 3 high)", choices=[('1', '1'), ('2', '2'), ('3', "3")], validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 
@@ -42,6 +57,56 @@ def hello_world():
 @app.route('/user/<name>')
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
+
+#PART 1
+@app.route('/artistform', methods=['GET', 'POST'])
+def artist_form():
+    return render_template('artistform.html')
+
+@app.route('/artistinfo', methods=['GET', 'POST'])
+def artist_info():
+    if request.method == "GET":
+        artist = request.args.get('artist')
+        base_url = 'https://itunes.apple.com/search?'
+        params_dict = {"term": artist, "media": "music", "entity": "musicTrack", "limit": "5"}
+        resp = requests.get(base_url, params=params_dict)
+        objects = json.loads(resp.text)['results']
+
+        return render_template('artist_info.html', objects=objects)
+
+#STUCK
+@app.route('/artistlinks')
+def artist_links():
+    return render_template('artist_links.html')
+
+@app.route('/specific/song/<artistname>')
+def specific_artist(artistname):
+    base_url = 'https://itunes.apple.com/search?'
+    params_dict = {"term": artistname, "media": "music", "entity": "musicTrack", "limit": "5"}
+
+
+#PART 3
+@app.route('/album_entry')
+def album_entry():
+    albumEntry = AlbumEntryForm()
+    return render_template('album_entry.html', form=albumEntry)
+
+@app.route('/album_result', methods=['GET', 'POST'])
+def album_result():
+    form = AlbumEntryForm()
+
+    if form.validate_on_submit():
+
+        album = form.album_name.data
+        like = form.like.data
+
+        return render_template('album_data.html', album_name=album, like=like)
+
+    flash(form.errors)
+
+    return redirect(url_for('album_entry'))
+
+
 
 
 if __name__ == '__main__':
